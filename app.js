e73106786aa256867a8861b1432a883ea33d226f
Brandon Herford <brandonherford@gmail.com>
2018-09-29 19:08:28 -0400

    Fix broken JS

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const displayJumble = document.querySelector('.result');
 const form = document.querySelector('form');
 const prodName1 = document.getElementById('prodOne');
 const prodName2 = document.getElementById('prodTwo');
-
 const jumble = document.createElement('h2');
 
 form.addEventListener('submit', addName);
@@ -12,20 +11,12 @@ function addName(e) {
 
   jumble.appendChild(document.createTextNode(prodName1.value));
   jumble.appendChild(document.createTextNode(prodName2.value));
-
   jumble.setAttribute('title', 'Your new product name jumble!');
   jumble.className = 'mixed';
 
-
-  //document.querySelector('div.result').appendChild(jumbleOutput);
-  var str = "My bologna has a first name, it's O S C A R.";
-
-  var shuffled = str.split('').sort(function(){return 0.5-Math.random()}).join('');
-
-
-console.log(jumbleOutput);
+  // Print name to screen
+  displayJumble.appendChild(jumble);
 
   e.preventDefault(); 
 }
-
 
