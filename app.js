3c0f85cf8e6d8961f8dc57555ff674d7529a8f47
Brandon Herford <brandonherford@gmail.com>
2018-09-29 23:10:07 -0400

    Update markup and JS

--- app.js
+++ app.js
@@ -9,12 +9,24 @@ form.addEventListener('submit', addName);
 
 function addName(e) {
 
-  jumble.appendChild(document.createTextNode(prodName1.value));
-  jumble.appendChild(document.createTextNode(prodName2.value));
+  newName = prodName1.value + prodName2.value;
+  jamble = newName.split('');
+
+  // Grabbed from jumble.appendChild(document.createTextNode(test.value));
+  const shuffle = ([...arr]) => {
+    let m = arr.length;
+    while (m) {
+      const i = Math.floor(Math.random() * m--);
+      [arr[m], arr[i]] = [arr[i], arr[m]];
+    }
+    return arr;
+  };
+
+  test = shuffle(jamble);
   jumble.setAttribute('title', 'Your new product name jumble!');
   jumble.className = 'mixed';
+  jumble.innerHTML = test.join('');
 
-  // Print name to screen
   displayJumble.appendChild(jumble);
 
   e.preventDefault(); 
