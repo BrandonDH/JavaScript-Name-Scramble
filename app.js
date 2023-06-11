08c3edce154e0b02cc7e6292e06ec77765afd035
Brandon Herford <brandonherford@gmail.com>
2023-06-11 15:57:28 -0400

    Clean house

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
 // Set variables for name jumble
-let displayJumble = document.querySelector('.result');
+let displayJumble = document.querySelector('#result');
 let form = document.querySelector('form');
 let prodName1 = document.getElementById('prodOne');
 let prodName2 = document.getElementById('prodTwo');
 let jumble = document.createElement('h1');
 
+// Handle submit event
 form.addEventListener('submit', addName);
 
+// Name mix logic
 function addName(event) {
-
   newName = prodName1.value + prodName2.value;
-  jamble = newName.split('');
 
+  // Shuffle logic
   let shuffle = ([...newWord]) => {
     let totalLetters = newWord.length;
     while (totalLetters) {
       let randomNum = Math.floor(Math.random() * totalLetters--);
-      console.log(newWord);
-      console.log(totalLetters);
-      console.log(newWord[totalLetters]);
-      console.log(newWord[randomNum]);
       [newWord[totalLetters], newWord[randomNum]] = [newWord[randomNum], newWord[totalLetters]];
     }
     return newWord;
   };
-
-  doTheDeed = shuffle(jamble);
+  
+  // Handle display
   jumble.setAttribute('title', 'Your new product name jumble!');
   jumble.className = 'mixed';
   jumble.innerHTML = doTheDeed.join('');
-
   displayJumble.appendChild(jumble);
-
   event.preventDefault(); 
 }
